f79b2976afb30c457fca5b9a86f39ccc4884b913
fyosu <josuahabeahan2016@gmail.com>
2024-11-23 15:57:50 +0700

    update

--- app/src/main/java/com/yosua/authentication/model/di/Injection.kt
+++ app/src/main/java/com/yosua/authentication/model/di/Injection.kt
@@ -5,11 +5,14 @@ import com.yosua.authentication.model.AppRepository
 import com.yosua.authentication.model.pref.UserPreference
 import com.yosua.authentication.model.pref.dataStore
 import com.yosua.authentication.model.remote.network.ApiConfig
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 
 object Injection {
     fun provide(context : Context): AppRepository{
-        val apiService = ApiConfig.getApiService()
         val userPref = UserPreference.getInstance(context.dataStore)
+        val user = runBlocking{userPref.getSession().first()}
+        val apiService = ApiConfig.getApiService(user.token)
         return AppRepository.getInstance(apiService, userPref)
     }
 }
--- app/src/main/java/com/yosua/authentication/model/remote/network/ApiConfig.kt
+++ app/src/main/java/com/yosua/authentication/model/remote/network/ApiConfig.kt
@@ -1,5 +1,6 @@
 package com.yosua.authentication.model.remote.network
 
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -9,11 +10,19 @@ object ApiConfig {
 
     private const val BASE_URL = "https://story-api.dicoding.dev/v1/"
 
-    fun getApiService() : ApiService {
+    fun getApiService(token : String) : ApiService {
         val loggingInterceptor =
             HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        val authInterceptor = Interceptor { chain ->
+            val req = chain.request()
+            val requestHeaders = req.newBuilder()
+                .addHeader("Authorization", "Bearer $token")
+                .build()
+            chain.proceed(requestHeaders)
+        }
         val client = OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
+            .addInterceptor(authInterceptor)
             .build()
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
